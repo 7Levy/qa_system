a74aeb9e2515b24fedbd3fc9ba8688299c6aae21
levy <greatfqc@gamil.com>
2020-03-06 17:12:55 +0800

    登录接口修改返回格式

--- QualityAssurance/qa_common/views.py
+++ QualityAssurance/qa_common/views.py
@@ -10,6 +10,7 @@ import datetime
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # Create your views here.
 from qa_common.extensions.jwt_auth import JwtAuthentication,create_token
+from qa_common.code import code
 class Login(APIView):
     authentication_classes = []
     def post(self,request,format=None):
@@ -20,9 +21,13 @@ class Login(APIView):
         s = serializers.UserLoginSerializer(queryset)
         login_time = datetime.datetime.now()
         if not user_object:
-            return Response({"status": "fail", "message": "账号或密码错误，请检查后重试！"})
+            return Response({'status':{'code':code.error_2000[0],'msg':code.error_2000[1]}})
         token = create_token({'id': user_object.id, 'user': user_object.account})
         queryset.last_login = login_time
         queryset.save()
-        # s.save()
-        return Response({"current_user":{"user":user,"user_id":s.data['id'],"login_time":login_time},"status": "success", "message": "登录成功","token":token,"token_prefix":""})
+        return Response({
+                            "current_user":{"user":user,"name":s.data['name'],"user_id":s.data['id'],"login_time":login_time,"department_name":s.data['department_name'],"department_id":s.data['department_id'],"type":s.data['type']},
+                            'status':{'code':code.sucess_code1001[0],'msg':code.sucess_code1001[1]},
+                            "token":token,"token_prefix":""
+
+                         })
